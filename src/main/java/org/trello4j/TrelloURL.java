171e45e151d50713015f6235d397387fb475b1db
Johan Mynhardt <johanmynhardt@gmail.com>
2012-11-18 21:54:50 +0200

    URL for attachment POST requests.

--- src/main/java/org/trello4j/TrelloURL.java
+++ src/main/java/org/trello4j/TrelloURL.java
@@ -42,6 +42,7 @@ public class TrelloURL {
 	public static final String CARD_MEMBERS_URL = "https://api.trello.com/1/cards/{0}/members";
 	public static final String CARD_POST_URL = "https://api.trello.com/1/cards";
 	public static final String CARD_POST_COMMENTS = "https://api.trello.com/1/cards/{0}/actions/comments";
+	public static final String CARD_POST_ATTACHMENTS = "https://api.trello.com/1/cards/{0}/attachments";
 	public static final String LIST_ACTIONS_URL = "https://api.trello.com/1/lists/{0}/action";
 	public static final String LIST_BOARD_URL = "https://api.trello.com/1/lists/{0}/board";
 	public static final String LIST_CARDS_URL = "https://api.trello.com/1/lists/{0}/cards";
